1a031bc6a07839d46be1e7e19b5a631602c919ad
kimochg <liucheng.cn@outlook.com>
2016-10-05 22:12:05 +0800

    AmapAPI encapsulated, jquery slider loaded. remove jquery add rcy:nouislider

--- client/gaodemap.js
+++ client/gaodemap.js
@@ -1,1 +1,72 @@
+var setting = Meteor.settings.public.amapKey;
+if (!setting) {
+  console.log('error', 'Please Add amap setting');
+}
+/**
+ * export AmapAPI
+ * inspired by dburles/meteor-google-maps
+ */
+AmapAPI = {
+  load : _.once(function(options) {
+    console.log('loading...');
+    options = _.extend({v : 1.3, key: setting}, options);
+    var params = _.map(options, function(value, key) {return key + '=' + value;}).join('&');
+    var script = document.createElement('script');
+    // script.type = 'text/javascript';
+    script.src = 'http://webapi.amap.com/maps?' + params + '&callback=initialize';
+    document.body.appendChild(script);
+    console.log('script appended');
+  }),
 
+  _loaded: new ReactiveVar(false),
+
+  loaded: function() {
+    return this._loaded.get();
+  },
+
+  initialize: function() {
+    console.log('initializing');
+    this._loaded.set(true);
+    console.log('loaded');
+    _.each(this.utilityLibraries, function(path) {
+      var script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = path;
+      document.body.appendChild(script);
+    });
+    // this.create();
+  },
+
+  utilityLibraries: [],
+
+  loadUtilityLibrary: function(path) {
+    this.utilityLibraries.push(path);
+  },
+
+  create: function(el) {
+    var self = this;
+    self.map = new AMap.Map(el, {
+      zoom:12
+      // center: [116.397428, 39.90923]
+    });
+    self.map.plugin(["AMap.ToolBar"], function() {
+      self.map.addControl(new AMap.ToolBar());
+    });
+    // self.map.on('click', function(){console.log('hello')});
+  },
+
+  map:{}
+
+};
+
+Template.gaodeMap.onRendered(function() {
+  var self = this;
+  self.autorun(function(c) {
+    console.log('gaode wait for loaded');
+    if (AmapAPI.loaded()) {
+      AmapAPI.create('map-canvas');
+      console.log('create amap successful');
+      c.stop();
+    }
+  });
+});
--- client/main.js
+++ client/main.js
@@ -1,43 +1,142 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+// import { ReactiveVar } from 'meteor/reactive-var';
+
 
 import './main.html';
 
-// Meteor.startup(() => {
-//
-// });
-
-var key = Meteor.settings.public.amapKey;
-console.log(key);
-
-Template.test.helpers({
-  amapKey : () => {
-    return key || 'no key setting'
-  }
-
+Meteor.startup(() => {
+  console.log('startup:call amap load');
+  AmapAPI.load({plugin: 'AMap.ArrivalRange'});
 });
 
-// Template.body.onCreated(() => {
-//   var map = new AMap.Map('container', {
-//       center: [117.000923, 36.675807],
-//       zoom: 6
-//   });
-// });
+/**
+ * helpers
+ */
+Template.mapApp.helpers({
+  time: function() {return Math.round(Session.get('slider'))}
+});
 
-// Template.hello.onCreated(function helloOnCreated() {
-//   // counter starts at 0
-//   this.counter = new ReactiveVar(0);
-// });
-//
-// Template.hello.helpers({
-//   counter() {
-//     return Template.instance().counter.get();
-//   },
-// });
-//
-// Template.hello.events({
-//   'click button'(event, instance) {
-//     // increment the counter when button is clicked
-//     instance.counter.set(instance.counter.get() + 1);
-//   },
-// });
+/**
+ * Render
+ */
+
+Template.mapApp.onRendered(function() {
+  var self = this;
+  self.autorun(function(c) {
+    if (AmapAPI.loaded()) {
+      // AmapAPI.map.on('click', function(e) {
+      //       var lngX=$("#lngX");
+      //       var latY=$("#latY");
+      //       lngX.val(e.lnglat.getLng());
+      //       latY.val(e.lnglat.getLat());
+      //       addCenterMarker(e.lnglat.getLng(),e.lnglat.getLat());
+      //       // addPolygon();
+      // });
+
+      /*
+       * create contextmenu
+       */
+      var centerMarker = null;
+      var mapContextMenu = new AMap.ContextMenu();
+      mapContextMenu.addItem('Add Location', function(e){
+        // console.log(centerMarker !== null);
+        if(centerMarker !== null){
+          centerMarker.setMap(null);
+        }
+        centerMarker = new AMap.Marker({
+            map: AmapAPI.map,
+            position: mapContextMenuPositon
+        });
+      }, 0);
+      /**
+       * bind contextmenu to rightclick
+       */
+      AmapAPI.map.on('rightclick', function(e){
+        mapContextMenu.open(AmapAPI.map, e.lnglat);
+        mapContextMenuPositon = e.lnglat;
+      });
+
+      /**
+       * [arrivalRange description]
+       * @type {AMap} Amap Object
+       */
+      var arrivalRange = new AMap.ArrivalRange(), x, y, t, v,centerMarker,polygonArray=[];
+      Session.set('isChanged',false);
+      Session.set("slider", 60);
+      self.$('#single-slider').noUiSlider({
+        start: Session.get("slider"),
+        connect: 'lower',
+        range: {
+          min: 1,
+          max: 100
+        }
+      }).on('slide', function (ev, val) {
+      // set real values on 'slide' event
+        Session.set('slider', val);
+        // console.log(Math.round(Session.get('slider')));
+      }).on('change', function (ev, val) {
+        // round off values on 'change' event
+        // Session.set('slider', [Math.round(val[0]), Math.round(val[1])]);
+      });
+
+      c.stop();
+    }
+  });
+});
+
+
+/**
+ * Functions
+ */
+function addCenterMarker(x,y){
+  if(typeof centerMarker !== null){
+    centerMarker.setMap(null);
+  }
+  centerMarker= new AMap.Marker({
+    map: AmapAPI.map,
+    position: [x,y]
+  });
+}
+
+
+  //添加多边形覆盖物
+function addPolygon() {
+    x=$("input[name='x']").val();
+    y=$("input[name='y']").val();
+    t=$("#t").val();
+    v=$("#v").val();
+    addCenterMarker(x,y);
+    arrivalRange.search([x,y],t, function(status,result){
+        delPolygon();
+        if(result.bounds){
+            for(var i=0;i<result.bounds.length;i++){
+               var polygon = new AMap.Polygon({
+                    map:map,
+                    fillColor:"#3366FF",
+                    fillOpacity:"0.4",
+                    strokeColor:"#00FF00",
+                    strokeOpacity:"0.5",
+                    strokeWeight:1
+                });
+                polygon.setPath(result.bounds[i]);
+                polygonArray.push(polygon);
+            }
+            map.setFitView();
+        }
+    },{
+        policy:v
+    });
+}
+  //
+  // function delPolygon(){
+  //     map.remove(polygonArray);
+  //     polygonArray=[];
+  // }
+  //
+
+  // addPolygon();
+  // window.setInterval(function(){//防止在移动滑动条时频繁触发请
+  //         if(isChanged){
+  //            addPolygon();
+  //            isChanged=false;
+  //        }
+  // },1000)
